d4ed4fdbf14ffc2e450aa6e42b12faab63ceb0df
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-23 17:47:05 +0200

    working on confirmation-dialog component

---
+++ src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+
+describe('confirmation dialog component specs', () => {
+  it('', () => {
+    // // Arrange
+    // // Act
+    // render(<ConfirmationDialogComponent {...props} />);
+    // // Assert
+    // const element = screen.getByRole('', {});
+    // expect(element).toBeInTheDocument();
+  });
+});

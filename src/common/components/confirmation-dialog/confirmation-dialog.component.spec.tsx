4f66ffb082d45e02e845ebfe85af14374d14bb0a
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-24 12:23:59 +0200

    1 test passed

--- src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
+++ src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { HtmlHTMLAttributes } from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
 describe('confirmation dialog component specs', () => {
-  it('', () => {
-    // // Arrange
-    // // Act
-    // render(<ConfirmationDialogComponent {...props} />);
-    // // Assert
-    // const element = screen.getByRole('', {});
-    // expect(element).toBeInTheDocument();
+  it('should be displayed when it feeds "isOpen" property with "true" value', () => {
+    // Arrange
+    const props = {
+      isOpen: true,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'test title',
+      labels: {
+        closeButton: 'test close btn',
+        acceptButton: 'test accept btn',
+      },
+    };
+
+    // Act
+    render(<ConfirmationDialogComponent {...props} />);
+
+    const dialogElement = screen.getByRole('dialog');
+    const closeButtonElement = screen.getByRole('button', {
+      name: props.labels.closeButton,
+    });
+    const acceptButtonElement = screen.getByRole('button', {
+      name: props.labels.acceptButton,
+    });
+
+    // Assert
+    expect(dialogElement).toBeInTheDocument();
+    expect(closeButtonElement).toBeInTheDocument();
+    expect(acceptButtonElement).toBeInTheDocument();
   });
 });

82eb957a8079d185a941fa4e969d075b28b95f04
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-24 16:36:40 +0200

    two tests passed

--- src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
+++ src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -1,4 +1,4 @@
-import React, { HtmlHTMLAttributes } from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
@@ -40,5 +40,43 @@ describe('confirmation dialog component specs', () => {
     expect(dialogContentElement).toBeInTheDocument();
     expect(closeButtonElement).toBeInTheDocument();
     expect(acceptButtonElement).toBeInTheDocument();
+  });
+
+  it('should not be displayed when it feeds "isOpen" property with "false" value', () => {
+    // Arrange
+    const props = {
+      isOpen: false,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'test title',
+      labels: {
+        closeButton: 'test close btn',
+        acceptButton: 'test accept btn',
+      },
+    };
+
+    // Act
+    render(
+      <ConfirmationDialogComponent {...props}>
+        <span>test children</span>
+      </ConfirmationDialogComponent>
+    );
+
+    const dialogElement = screen.queryByRole('dialog');
+    const dialogTitleElement = screen.queryByText(props.title);
+    const dialogContentElement = screen.queryByText('test children');
+    const closeButtonElement = screen.queryByRole('button', {
+      name: props.labels.closeButton,
+    });
+    const acceptButtonElement = screen.queryByRole('button', {
+      name: props.labels.acceptButton,
+    });
+
+    // Assert
+    expect(dialogElement).toEqual(null);
+    expect(dialogTitleElement).toEqual(null);
+    expect(dialogContentElement).toEqual(null);
+    expect(closeButtonElement).toEqual(null);
+    expect(acceptButtonElement).toEqual(null);
   });
 });

e1ab0ee0e89b859fd20f3083a48b583b9b42eff4
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-25 21:03:40 +0200

    working on spinner

--- src/common/components/spinner/spinner.component.spec.tsx
+++ src/common/components/spinner/spinner.component.spec.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
+import { renderHook } from '@testing-library/react-hooks';
 import { render, screen } from '@testing-library/react';
 import { SpinnerComponent } from './spinner.component';
-import * as PromiseTracker from 'react-promise-tracker';
+import { usePromiseTracker } from 'react-promise-tracker';
 
 describe('spinner component spec', () => {
   it('should be displayed when "promiseInProgress" is "true"', () => {
     // Arrange
-    jest
-      .spyOn(PromiseTracker, 'usePromiseTracker')
-      .mockImplementation(() => ({ promiseInProgress: true }));
+    const { result } = renderHook(() => usePromiseTracker());
+    result.current.promiseInProgress = true;
+    expect(result.current.promiseInProgress).toEqual(true);
 
     // Act
     render(<SpinnerComponent />);
 
-    const element = screen.getByRole('status');
     // Assert
-    expect(element).toBeInTheDocument();
   });
 });

62a5656abd1967138a73f00bd28977f01d31ce7e
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-28 20:03:39 +0200

    working on login scene

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,5 +1,25 @@
 describe('login scene specs', () => {
-  it('should visit the login page', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('user input should get the focus when clicking on it', () => {
+    // Arrange
+
+    // Act
+    cy.get('input[name="user"]').click();
+
+    // Asert
+    cy.get('input[name="user"]').should('have.focus');
+  });
+
+  it('user input should get the focus when clicking on it', () => {
+    // Arrange
+
+    // Act
+    cy.get('input[name="password"]').click();
+
+    // Asert
+    cy.get('input[name="password"]').should('have.focus');
   });
 });

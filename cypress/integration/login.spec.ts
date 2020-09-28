eac4e6e52bdbe3318533d051693170e738acc83a
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-28 20:20:41 +0200

    working on login scene

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -3,7 +3,7 @@ describe('login scene specs', () => {
     cy.visit('/');
   });
 
-  it('user input should get the focus when clicking on it', () => {
+  it('"user" input should get the focus when clicking on it', () => {
     // Arrange
 
     // Act
@@ -13,7 +13,7 @@ describe('login scene specs', () => {
     cy.get('input[name="user"]').should('have.focus');
   });
 
-  it('user input should get the focus when clicking on it', () => {
+  it('"password" input should get the focus when clicking on it', () => {
     // Arrange
 
     // Act
@@ -21,5 +21,33 @@ describe('login scene specs', () => {
 
     // Asert
     cy.get('input[name="password"]').should('have.focus');
+  });
+
+  it('"user" input should show error message "Debe informar el campo" when blur after clicking on it', () => {
+    // Arrange
+    const errorMsg = 'Debe informar el campo';
+
+    // Act
+    cy.get('input[name="user"]').click();
+    cy.get('input[name="user"]').blur();
+
+    // Asert
+    cy.get(
+      'p[class="MuiFormHelperText-root MuiFormHelperText-contained Mui-error"]'
+    ).should('have.text', errorMsg);
+  });
+
+  it('"password" input should show error message "Debe informar el campo" when blur after clicking on it', () => {
+    // Arrange
+    const errorMsg = 'Debe informar el campo';
+
+    // Act
+    cy.get('input[name="password"]').click();
+    cy.get('input[name="password"]').blur();
+
+    // Asert
+    cy.get(
+      'p[class="MuiFormHelperText-root MuiFormHelperText-contained Mui-error"]'
+    ).should('have.text', errorMsg);
   });
 });
